ebf7aedac8b2fd2f9ca0b9a901a2ca5bf5c1bde7
Facu6M <facu6p@gmail.com>
2023-09-06 03:33:54 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+// components
+import Header from './components/Header'
+import Main from "./components/Main"
+import Footer from './components/Footer'
+
+function App() {
+
+  return (
+    <>
+<Header />
+<Main />
+<Footer />
+</>
+    )
+}
+
+export default App
